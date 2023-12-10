edee755cf18cf2a3f9d4713cbea9d72aebdf1fee
fuadps <fuadpls@gmail.com>
2023-12-10 23:32:23 +0800

    set nav

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -21,8 +21,12 @@ export default function Authenticated({ user, header, children }) {
                             </div>
 
                             <div className="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                                <NavLink href={route('dashboard')} active={route().current('dashboard')}>
-                                    Dashboard
+                                <NavLink href={route('companies.index')} active={route().current('companies.index')}>
+                                    Companies
+                                </NavLink>
+
+                                <NavLink href={route('employees.index')} active={route().current('employees.index')}>
+                                    Employees
                                 </NavLink>
                             </div>
                         </div>
